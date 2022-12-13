3de4709a677be3e357af5c739d3f5aa00c635f3b
Monse <monserrat.gonzalez95@gmail.com>
2022-12-13 15:53:10 -0600

    cambios

--- __manifest__.py
+++ __manifest__.py
@@ -19,16 +19,20 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','contacts','crm'],
+    'depends': [
+            'direcciones','contacts_extend','base'
+        ],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
+        'security/file_security.xml',
+
         'views/views.xml',
-        'views/templates.xml',
+        # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
 }
--- models/models.py
+++ models/models.py
@@ -20,7 +20,6 @@ class CrmLead(models.Model):
     nombre = fields.Char()
     ap_paterno = fields.Char()
     ap_materno = fields.Char()
-    #rfc = fields.Char()
     banco = fields.Selection([
         ('Afirme', 'Afirme'),
         ('Autofin', 'Autofin'),
@@ -42,18 +41,16 @@ class CrmLead(models.Model):
 
     ], string='banco')
     
-    zoho_files = fields.One2many('crm.zoho','crm_id',string="Archivos")
+    zoho_files = fields.One2many('crm.zoho','crm_id',string="Caratula Zoho")
 
    
     estado = fields.Many2one('direcciones.estados',string="Estado")
     municipio = fields.Many2one('direcciones.municipios', string="Municipio")
-    archivo_base64 = fields.Char()
     @api.onchange('estado')
     def filter_munic(self):
         return {'domain': {'municipio': [('id_estado','=', self.estado.id)]}}
 
     """Campos para insertar o actualizar una oficina"""
-    check_zoho_id = fields.Boolean(string="¿Cuentas con Zoho ID?")
     zoho_id = fields.Char()
     nombre_oficina = fields.Char('Nombre de la oficina')
     telefono_oficina = fields.Char('Télefono de la oficina')
@@ -64,53 +61,10 @@ class CrmLead(models.Model):
     cp_oficina = fields.Char('Código Postal')
     correo_oficina = fields.Char('Correo electrónico')
     sitio_web_oficina = fields.Char('Sitio web')
-    #importe_zoho = fields.Char('Importe')
     respuesta_err = fields.Char()
+    caratula_zoho = fields.Binary('Caratula Zoho')
+
     """Funciones para las distintas operaciones permitidas en zoho"""
-
-    # def inserta_oficina(self):
-    #     url = "https://sandbox.zohoapis.com/crm/v2/functions/masterbroker_inserta_oficina_v1/actions/execute?auth_type=apikey&master_broker_token=chlestiustiayiecrlucroewiaphlar2&zapikey=1003.0493b2f5bf95786ae1461418498f0f32.42839246fcebd5e3f45ccd7b1cdebad7"
-    #     if not self.nombre_oficina :
-    #         raise UserError("Nombre de la oficina no definido")
-    #     if not self.telefono_oficina :
-    #         raise UserError("Telefono de la oficina no definida")
-    #     if not self.calle_num_oficina :
-    #         raise UserError("Calle y número de la oficina no definido")
-    #     if not self.colonia_oficina :
-    #         raise UserError("Colonia de la oficina no definido")
-    #     if not self.delegacion_oficina:
-    #         raise UserError("Delegación de la no definido")
-    #     if not self.estado_oficina :
-    #         raise UserError("Estado Oficina no definido")
-    #     if not self.cp_oficina :
-    #         raise UserError("Codigo Postal Oficina definido")
-    #     if not self.correo_oficina:
-    #         raise UserError("Correo de la oficina no definido")
-    #     if not self.sitio_web_oficina:
-    #         raise UserError("Sitio Web de la oficina no definida")
-
-    #     payload = json.dumps({
-    #     {
-    #         "zoho_id": self.zoho_id if self.zoho_id else "",
-    #         "oficina_nombre": self.nombre_oficina,
-    #         "oficina_telefono":self.telefono_oficina,
-    #         "oficina_calle_y_no": self.calle_num_oficina,
-    #         "oficina_colonia": self.colonia_oficina,
-    #         "oficina_delegacion_municipio": self.delegacion_oficina,
-    #         "oficina_estado": self.estado_oficina,
-    #         "oficina_codigo_postal": self.cp_oficina,
-    #         "oficina_correo_electronico": self.correo_oficina,
-    #         "oficina_sitio_web": self.sitio_web_oficina,
-    #     }
-    #     })
-    #     headers = {
-    #     'zapikey': '1003.cc7584ca319e65e957f32ce61e2f1502.fbabf6196572fa59f4c3fc203c967318',
-    #     'Content-Type': 'application/json'
-    #     }
-
-    #     response = requests.request("POST", url, headers=headers, data=payload)
-
-    #     logger.info(response.text) 
 
     def inserta_operacion(self):
         try :
@@ -155,12 +109,10 @@ class CrmLead(models.Model):
             }
             response = requests.request("POST", url, headers=headers, data=payload)
             response = json.loads(response.content,strict=False)
-            # respuesta = (response.content)
-            # respuesta= json.load(respuesta)
             if 'error' in response: 
                 raise UserError(response)
             if 'pdf' in response:
-                archivo=response['pdf']        
+                archivo=response['pdf']
                 self.env['crm.zoho'].sudo().create({'crm_id': crm.id, 'archivo' :archivo, 'fecha_creacion': datetime.now().astimezone(timezone('America/Mexico_City'))})
             else:
                 raise UserError(response)
@@ -174,7 +126,7 @@ class archivosZoho(models.Model):
     _order='create_date desc'
 
     crm_id = fields.Many2one('crm.lead',string='Oportunidad')
-    archivo = fields.Binary('Archivo')
+    archivo = fields.Binary('Zoho Caratula')
     filename_archivo = fields.Char()
     fecha_creacion = fields.Date('Fecha de creación')
     
